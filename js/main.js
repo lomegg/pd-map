b2d22e306577a7d0922ecf0bc016a408f539c3a1
lomegg <unrelated@ya.ru>
2017-04-27 02:42:02 +0200

    some organisation

--- js/main.js
+++ js/main.js
@@ -1,59 +1,334 @@
+// server list
+window.serverModesList = {
+    "kraken1":	"KRAKEN",
+    "team1":	"TEAM",
+    "team8":	"TEAM",
+    "team10":	"TEAM",
+    "arena1":	"ARENA",
+    "arena2":	"ARENA",
+    "arena3":	"ARENA",
+    "arena4":	"ARENA",
+    "arena5":	"ARENA",
+    "arena6":	"ARENA",
+    "arena7":	"ARENA",
+    "arena8":	"ARENA",
+    "arena9":	"ARENA",
+    "arena10":	"ARENA",
+    "arena11":	"ARENA",
+    "arena12":	"ARENA",
+    "arena13":	"ARENA",
+    "arena14":	"ARENA",
+    "arena15":	"ARENA",
+    "arena16":	"ARENA",
+    "arena17":	"ARENA",
+    "arena18":	"ARENA",
+    "arena19":	"ARENA",
+    "arena20":	"ARENA",
+    "fatboy1":	"FATBOY-ARENA",
+    "fatboy2":	"FATBOY-ARENA",
+    "fatboy3":	"FATBOY-ARENA",
+    "fatboy4":	"FATBOY-ARENA",
+    "fatboy5":	"FATBOY-ARENA",
+    "fatboy6":	"FATBOY-ARENA",
+    "fatboy7":	"FATBOY-ARENA",
+    "fatboy8":	"FATBOY-ARENA",
+    "fatboy9":	"FATBOY-ARENA",
+    "fatboy10":	"FATBOY-ARENA",
+    "fatboy11":	"FATBOY-ARENA",
+    "fatboy12":	"FATBOY-ARENA",
+    "fatboy18":	"FATBOY-ARENA",
+    "fatboy13":	"FATBOY-ARENA",
+    "fatboy15":	"FATBOY-ARENA",
+    "fatboy21":	"FATBOY-ARENA",
+    "fatboy23":	"FATBOY-ARENA",
+    "fatboy16":	"FATBOY-ARENA",
+    "fatboy17":	"FATBOY-ARENA",
+    "fatboy34":	"FATBOY-ARENA",
+    "fatboy35":	"FATBOY-ARENA",
+    "fatboy36":	"FATBOY-ARENA",
+    "fatboy37":	"FATBOY-ARENA",
+    "fatboy38":	"FATBOY-ARENA",
+    "fatboy39":	"FATBOY-ARENA",
+    "fatboy40":	"FATBOY-ARENA",
+    "fatboy41":	"FATBOY-ARENA",
+    "fatboy42":	"FATBOY-ARENA",
+    "fatboy43":	"FATBOY-ARENA",
+    "fatboy44":	"FATBOY-ARENA",
+    "fatboy45":	"FATBOY-ARENA",
+    "fatboy19":	"FATBOY-ARENA",
+    "fatboy14":	"FATBOY-ARENA",
+    "fatboy20":	"FATBOY-ARENA",
+    "fatboy22":	"FATBOY-ARENA",
+    "fatboy24":	"FATBOY-ARENA",
+    "fatboy25":	"FATBOY-ARENA",
+    "fatboy26":	"FATBOY-ARENA",
+    "fatboy27":	"FATBOY-ARENA",
+    "fatboy28":	"FATBOY-ARENA",
+    "fatboy29":	"FATBOY-ARENA",
+    "fatboy30":	"FATBOY-ARENA",
+    "fatboy31":	"FATBOY-ARENA",
+    "fatboy32":	"FATBOY-ARENA",
+    "fatboy33":	"FATBOY-ARENA",
+    "fatboy46":	"FATBOY-ARENA",
+    "fatboy47":	"FATBOY-ARENA",
+    "snakerdish1":	"SNAKERDISH",
+    "crazy1":	"CRAZY-FFA",
+    "exp1":	"EXPERIMENTAL",
+    "fastfood1":	"FASTFOOD",
+    "fastfood2":	"FASTFOOD",
+    "viruswarsffa6":	"VIRUSWARSFFA",
+    "blackhole1":	"BLACKHOLE",
+    "blackhole4":	"BLACKHOLE",
+    "blackhole5":	"BLACKHOLE",
+    "blackhole6":	"BLACKHOLE",
+    "blackhole7":	"BLACKHOLE",
+    "blackhole8":	"BLACKHOLE",
+    "blackhole9":	"BLACKHOLE",
+    "solar":	"FFA",
+    "galaxy":	"FFA",
+    "earth":	"FFA",
+    "universe":	"FFA",
+    "russia":	"FFA",
+    "nolimit":	"FFA",
+    "tunnel":	"FFA",
+    "sky":	"FFA",
+    "oneshotffa1":	"ONESHOT",
+    "oneshotmegasplit1":	"ONESHOT",
+    "oneshotmegasplit20":	"ONESHOT",
+    "space1":	"SPACE",
+    "crazysplit1":	"CRAZYSPLIT",
+    "crazysplit2":	"CRAZYSPLIT",
+    "crazysplit3":	"CRAZYSPLIT",
+    "crazysplit4":	"CRAZYSPLIT",
+    "crazysplit5":	"CRAZYSPLIT",
+    "crazysplit6":	"CRAZYSPLIT",
+    "crazysplit7":	"CRAZYSPLIT",
+    "crazysplit8":	"CRAZYSPLIT",
+    "crazysplit9":	"CRAZYSPLIT",
+    "crazysplit10":	"CRAZYSPLIT",
+    "crazysplit11":	"CRAZYSPLIT",
+    "crazysplit12":	"CRAZYSPLIT",
+    "crazysplit13":	"CRAZYSPLIT",
+    "crazysplit14":	"CRAZYSPLIT",
+    "crazysplit15":	"CRAZYSPLIT",
+    "crazysplit16":	"CRAZYSPLIT",
+    "crazysplit17":	"CRAZYSPLIT",
+    "crazysplit18":	"CRAZYSPLIT",
+    "crazysplit19":	"CRAZYSPLIT",
+    "crazysplit20":	"CRAZYSPLIT",
+    "crazysplit21":	"CRAZYSPLIT",
+    "eatforspeed1":	"EATFORSPEED",
+    "eatforspeed5":	"EATFORSPEED",
+    "special1":	"SPECIAL",
+    "special2":	"SPECIAL",
+    "special3":	"SPECIAL",
+    "special4":	"SPECIAL",
+    "special5":	"SPECIAL",
+    "megasplit1":	"MEGASPLIT",
+    "megasplit2":	"MEGASPLIT",
+    "megasplit3":	"MEGASPLIT",
+    "megasplit4":	"MEGASPLIT",
+    "megasplit10":	"MEGASPLIT",
+    "megasplit27":	"MEGASPLIT",
+    "megasplit40":	"MEGASPLIT",
+    "megasplit5k1":	"MEGASPLIT5K",
+    "megasplit5k2":	"MEGASPLIT5K",
+    "megasplit5k3":	"MEGASPLIT5K",
+    "megasplit5k4":	"MEGASPLIT5K",
+    "megasplit5k5":	"MEGASPLIT5K",
+    "megasplit5k6":	"MEGASPLIT5K",
+    "megasplit5k7":	"MEGASPLIT5K",
+    "megasplit5k8":	"MEGASPLIT5K",
+    "megasplit5k9":	"MEGASPLIT5K",
+    "megasplit5k10":	"MEGASPLIT5K",
+    "megasplit5k18":	"MEGASPLIT5K",
+    "hardcore1":	"HARDCORE",
+    "hardcore2":	"HARDCORE",
+    "hardcore3":	"HARDCORE",
+    "hardcore5":	"HARDCORE",
+    "hardcore6":	"HARDCORE",
+    "hardcore7":	"HARDCORE",
+    "hardcore8":	"HARDCORE",
+    "hardcore9":	"HARDCORE",
+    "hardcore10":	"HARDCORE",
+    "hardcore12":	"HARDCORE",
+    "hardcore26":	"HARDCORE",
+    "extreme7":	"HARDCORE",
+    "hardcoreexp1":	"HARDCOREEXP",
+    "hardcoreexp2":	"HARDCOREEXP",
+    "hardcoreexp3":	"HARDCOREEXP",
+    "hardcoreexp4":	"HARDCOREEXP",
+    "hardcoreexp5":	"HARDCOREEXP",
+    "hardcoreexp6":	"HARDCOREEXP",
+    "hardcoreexp7":	"HARDCOREEXP",
+    "hardcoreexp8":	"HARDCOREEXP",
+    "hardcoreexp9":	"HARDCOREEXP",
+    "hardcoreexp10":	"HARDCOREEXP",
+    "hardcoreexp21":	"HARDCOREEXP",
+    "hardcoreexp22":	"HARDCOREEXP",
+    "hardcoreexp41":	"HARDCOREEXP",
+    "hardcoreexp50":	"HARDCOREEXP",
+    "zombie1":	"ZOMBIE-FFA",
+    "dm1":	"DEATHMATCH",
+    "luna":	"FFA",
+    "venera":	"FFA",
+    "saturn":	"FFA",
+    "slowserver":	"FFA",
+    "paris":	"FFA",
+    "turkey":	"FFA",
+    "amsterdam":	"FFA",
+    "moscow":	"FFA",
+    "canada":	"FFA",
+    "london":	"FFA",
+    "siberia":	"FFA",
+    "kiev":	"FFA",
+    "minsk":	"FFA",
+    "usa1-newyork":	"FFA",
+    "usa2-newyork":	"FFA",
+    "usa3-newyork":	"FFA",
+    "singapore1":	"FFA",
+    "toronto":	"FFA",
+    "clanhousew.w":	"CLANHOUSE",
+    "clanhouse18-25":	"CLANHOUSE",
+    "clanhousefff":	"CLANHOUSE",
+    "clanhousefbi":	"CLANHOUSE",
+    "clanhouse•••":	"CLANHOUSE",
+    "clanhousedsm":	"CLANHOUSE",
+    "clanhousezzz":	"CLANHOUSE",
+    "clanhouse72":	"CLANHOUSE",
+    "clanhousezaw":	"CLANHOUSE",
+    "clanhousepvl":	"CLANHOUSE",
+    "clanhousedead":	"CLANHOUSE",
+    "clanhouser2b":	"CLANHOUSE",
+    "clanhouseng":	"CLANHOUSE",
+    "clanhouseyolka":	"CLANHOUSE",
+    "clanhousewin":	"CLANHOUSE",
+    "clanhousefan":	"CLANHOUSE",
+    "clanhouseup":	"CLANHOUSE",
+    "clanhouserzr":	"CLANHOUSE",
+    "clanhousehawk":	"CLANHOUSE",
+    "clanhousezogo":	"CLANHOUSE",
+    "clanhousegyg":	"CLANHOUSE",
+    "clanhousepav":	"CLANHOUSE",
+    "clanhousepride":	"CLANHOUSE",
+    "clanhouseaob":	"CLANHOUSE",
+    "clanhousenoteam1":	"CLANHOUSE",
+    "clanhousetournament":	"CLANHOUSE",
+    "clanhousetest":	"CLANHOUSE"
+};
+
+
+/*========================++  CUT BELOW THIS LINE   ++======================================*/
+
 //  Trigger map toggle on keydown
 
-$(document).on("keydown.", function(event){
+$(document).on("keydown", function(event){
     //console.log(event.which);
     //console.log(event);
 
+    // check if server list present
+    if (!window.serverModesList){ window.serverModesList = smc.getServerModeList();}
+
+    // toggle map if M pressed
     if (event.which == 77){
-        // M pressed
-        console.log('M pressed');
-
-        // toggle map
-        toggleServerMap();
-
+        smc.toggleServerMap();
     }
 });
+
+// bind map removal to close button
 
 $(document).on('click', '#server-map .map-controls a', function(e){
-    destroyServerMap();
+    smc.destroyMap();
 });
 
 
-function toggleServerMap(){
-    if ($('#server-map').length){
-        console.log('map is open');
-        destroyServerMap();
-    } else {
-        console.log('map is closed');
-        createServerMap();
-    }
-}
+// server map creator
+var smc = {
 
-
-function createServerMap(){
-    console.log('creating map');
-
-    var mapControls =
-        "<div class='map-controls'>" +
+    // Map drawing
+    
+    createMap: function(){
+        smc.getJson(function(json){
+            // sort json
+            var winners = smc.groupByServerType(json.winners);
+            smc.drawMap(json, winners);
+        });
+    },
+    destroyMap: function(){
+        $( "#server-map" ).remove();
+    },
+    drawMap: function(originalJson, winners){
+        var mapControls =
+            "<div class='map-controls'>" +
             '<a href="javascript:void(0)">'+
-                'Press "M" to close map' +
+            'Press "M" to close map' +
             '</a>' +
-        "</div>";
+            "</div>";
 
-
-    var map =
-        "<div id='server-map'>" +
+        var map =
+            "<div id='server-map'>" +
             mapControls +
             "<div class='map-inner'>" +
             "</div>" +
-        "</div>";
-    map = $( map );
+            "</div>";
+        map = $( map );
+        $('body').append(map);
+    },
+    toggleServerMap: function(){
+        if ($('#server-map').length){
+            console.log('map is open');
+            smc.destroyMap();
+        } else {
+            console.log('map is closed');
+            smc.createMap();
+        }
+    },
 
-    $('body').append(map);
-}
+    // JSON operations
+    
+    getJson: function(callback){
+        // TODO: replace with absolute path
+
+        $.getJSON('shop_winners/4.json', function (obj) {
+            return callback(obj);
+        });
+    },
+    getServerModeList: function(){
+        // get server modes list by server name
+
+        var list = {};
+        $('.server-list .server-item').toArray().forEach(
+            function(el){
+                list[el.getAttribute('autorun')] = el.getAttribute('mode');
+            }
+        );
+        return list;
+    },
+    groupByServerType: function(winners){
+        // group winners by server type
+        var sorted = {}, mode;
+
+        for (var server in winners) {
+            if (winners.hasOwnProperty(server)) {
+                // do stuff
+
+                mode = window.serverModesList[server];
+
+                if (!sorted[mode]){
+                    sorted[mode] = [];
+                }
+
+                sorted[mode].push(winners[server]);
+            }
+        }
+
+        console.log(sorted);
+        return sorted;
+    }
+};
 
 
-function destroyServerMap(){
-    console.log('destroying map');
-    $( "#server-map" ).remove();
-}
+
+
+
+
